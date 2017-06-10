fbaf6291a6f73e2f0c0382f06e7ff90fdac1be3d
cyrilf <cyrilf7x@gmail.com>
2017-06-10 19:52:31 +0200

    Use and draw basic color in the canvas

--- App.js
+++ App.js
@@ -1,23 +1,40 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React from 'react'
+import { StyleSheet, Text, View } from 'react-native'
+
+import Canvas from './app/components/Canvas'
+
+const renderCanvas = (canvas) => {
+  const ctx = canvas.getContext('2d')
+  ctx.fillStyle = 'green'
+  ctx.fillRect(0, 0, canvas.width, canvas.height)
+}
 
 export default class App extends React.Component {
   render() {
     return (
       <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
-        <Text>Changes you make will automatically reload.</Text>
+        <Text style={styles.text}>Yo maeen! Open up App.js to start working on your app!</Text>
+        <Text style={styles.text}>Changes you make will automatically reload.</Text>
         <Text>Shake your phone to open the developer menu.</Text>
+
+        <Canvas
+          context={{message: 'Hello!'}}
+          render={renderCanvas}
+          style={{height: 200, width: 200}}
+        />
       </View>
-    );
+    )
   }
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: 'powderblue',
     alignItems: 'center',
     justifyContent: 'center',
   },
-});
+  text: {
+    color: 'white',
+  }
+})
