2440db0a4abb3c59245aa2f43b1e5aa187f8c1d2
cyrilf <cyrilf7x@gmail.com>
2017-06-10 19:51:17 +0200

    Create a simple react-native Canvas component

---
+++ app/components/Canvas/Canvas.js
@@ -0,0 +1,20 @@
+import React from 'react'
+import { WebView } from 'react-native'
+
+const Canvas = ({ context, render, style }) => {
+  const contextString = JSON.stringify(context)
+  const renderString = render.toString()
+  const jsCode = `var canvas = document.querySelector("canvas");(${renderString}).call(${contextString}, canvas)`
+
+  return (
+    <WebView
+      automaticallyAdjustContentInsets={false}
+      contentInset={{top: 0, right: 0, bottom: 0, left: 0}}
+      source={{html: '<style>*{margin:0;padding:0}canvas{transform:translateZ(0)}</style><canvas></canvas>'}}
+      injectedJavaScript={jsCode}
+      style={style}
+    />
+  )
+}
+
+export default Canvas
